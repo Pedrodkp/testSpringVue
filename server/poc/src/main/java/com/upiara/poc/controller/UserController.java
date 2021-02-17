31d2423b67c7a316229644ef1c7a7819c79cfa7c
Pedro Pereira <pedro.pereira@upiara.com>
2021-02-17 00:16:23 -0300

    gets e post user

--- server/poc/src/main/java/com/upiara/poc/controller/UserController.java
+++ server/poc/src/main/java/com/upiara/poc/controller/UserController.java
@@ -3,10 +3,15 @@ package com.upiara.poc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.upiara.poc.exception.ResourceNotFoundException;
 import com.upiara.poc.model.User;
 import com.upiara.poc.repository.UserRepository;
 
@@ -19,5 +24,20 @@ public class UserController {
 	@GetMapping("/users")
 	public List <User> getAllUsers() {
 		return userRepository.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity <User> getUserById(@PathVariable(value = "id") Long userId)
+	throws ResourceNotFoundException {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Usuário não existe para o id: "+userId));
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		user.setId(null); //ORA-32575
+		userRepository.save(user);
+		return userRepository.findTopByOrderByIdDesc();
 	}
 }
--- server/poc/src/main/java/com/upiara/poc/model/User.java
+++ server/poc/src/main/java/com/upiara/poc/model/User.java
@@ -2,9 +2,14 @@ package com.upiara.poc.model;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,11 +19,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "USERS")
+@Table(name = "VW_USERS")
 public class User {
-	@Id
+	@Id 
+	@GenericGenerator(name = "UseExistingIdOtherwiseGenerateUsingIdentity", strategy = "com.upiara.poc.generator.UseExistingIdOtherwiseGenerateUsingIdentity")
+	@GeneratedValue(generator = "UseExistingIdOtherwiseGenerateUsingIdentity")
+	@Column(name = "id", unique = true, nullable = false, columnDefinition = "serial")
 	private Long id;
 	private String name;
 	private Date birthday;
-	private Long language_id;
+	private String language_code;
+	private String language_description;
+	
+	public void setId(Long ig) {
+		this.id = 1L; //ORA-32575
+	}
 }
